6999684c5dcd82d4be5f3a8e85c7c3afe06f9ab2
L1oid <lloidtm@gmail.com>
2024-03-04 11:33:04 +0700

    upd education

--- genetics-website-frontend/src/view/components/education/course/component.js
+++ genetics-website-frontend/src/view/components/education/course/component.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 
 import course_list from "../../../../data/course_list";
+import ed_materials_list from "../../../../data/ed_materials_list";
 import './style.css';
 
 import BreadcrumpComponent from "../../common/breadcrump/component";
@@ -30,7 +31,11 @@ function CourseComponent() {
                     <div className="course-column-1">
                         <div className="course-navigation">
                             <p className="course-navigation-title">Навигация</p>
-                            <p className="course-navigation-item-title materials">Образовательные материалы</p>
+                            <p className="course-navigation-item-title">
+                                <Link className="course-navigation-item-list-href"
+                                      to={"/education"}>Образовательные материалы
+                                </Link>
+                            </p>
                             <p className="course-navigation-item-title"
                                onClick={() => setItemListStatus(!itemListStatus)}>
                                 Общедоступные курсы
@@ -50,7 +55,15 @@ function CourseComponent() {
                         </div>
                     </div>
                     <div className="course-column-2">
-                        Курс
+                        <div className="course-content-container">
+                            <p className="course-content-title">Образовательные материалы</p>
+                            {ed_materials_list.map((material, materialIndex) => (
+                                <div className="course-content-item">
+                                    <span className="material-symbols-outlined icon">picture_as_pdf</span>
+                                    <a href={material.url} className="course-content-item-name">{material.name}</a>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ genetics-website-frontend/src/view/components/education/courseNavigation/component.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './style.css';
+import {Link} from "react-router-dom";
+
+function CourseNavigationComponent(props) {
+    return (
+        <div className="course-navigation">
+            <p className="course-navigation-title">Навигация</p>
+            <p className="course-navigation-item-title">
+                <Link className="course-navigation-item-list-href"
+                      to={"/education"}>Образовательные материалы
+                </Link>
+            </p>
+            <p className="course-navigation-item-title"
+               onClick={() => setItemListStatus(!itemListStatus)}>
+                Общедоступные курсы
+            </p>
+            <div
+                className={itemListStatus ? "course-navigation-item-list" : "course-navigation-item-list active"}>
+                {course_list.map((course, courseIndex) => (
+                    course.private === false && (
+                        <p className="course-navigation-item-list-title">
+                            <Link className="course-navigation-item-list-href"
+                                  to={"/education/" + course.id}>{course.title}</Link>
+                        </p>
+                    )
+                ))}
+            </div>
+            <div className="course-navigation-item-title">Мои курсы</div>
+        </div>
+    )
+}
+
+export default CourseNavigationComponent;
