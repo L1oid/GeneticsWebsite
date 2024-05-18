5066eb8b37a58825901e7a4938cf65f36d392c31
L1oid <lloidtm@gmail.com>
2024-05-18 21:47:00 +0700

    upd errors add quest

--- genetics-website-frontend/src/state/consts/api.js
+++ genetics-website-frontend/src/state/consts/api.js
@@ -1,5 +1,6 @@
 export const api = {
-    url: "http://genetic.kemsu.ru/genetics/",
+    //url: "http://genetic.kemsu.ru/genetics/",
+    url: "http://webschedulekemsu.ddnsking.com:2023/GeneticsWebBackend-1.0-SNAPSHOT/",
     authorization: "api/users/submissions",
     changePassword: "api/users/",
     registrationUser: "api/users",
--- genetics-website-frontend/src/state/slices/content/errorHandlers.js
+++ genetics-website-frontend/src/state/slices/content/errorHandlers.js
@@ -2,32 +2,56 @@ import {
     CONTENT_ADD_TOO_MANY_FILES,
     CONTENT_ARTICLE_DOESNT_EXIST,
     CONTENT_COULD_NOT_GET_USER_ID,
-    CONTENT_COULD_NOT_INITIALIZE_ARTICLES, CONTENT_DATE_SHOULD_BE_SPECIFIED,
+    CONTENT_COULD_NOT_INITIALIZE_ARTICLES,
+    CONTENT_DATE_SHOULD_BE_SPECIFIED,
     CONTENT_DELETE_MEDIA_ID_IS_0,
-    CONTENT_DELETE_TOO_MANY_FILES, CONTENT_DESCRIPTION_SHOULD_BE_SPECIFIED,
+    CONTENT_DELETE_TOO_MANY_FILES,
+    CONTENT_DESCRIPTION_SHOULD_BE_SPECIFIED,
     CONTENT_EDIT_INTERNAL_ERROR,
     CONTENT_EMPTY_ARTICLE_FIELDS,
     CONTENT_FAILED_TO_SAVE_ARTICLE,
     CONTENT_FILES_IS_NOT_SUPPORTED_FORMAT,
     CONTENT_FILES_SIZE_TOO_MANY,
-    CONTENT_IN_FILE_LIST_FILES_LACKS_FILE_NAME, CONTENT_INCORRECT_REQUEST,
+    CONTENT_IN_FILE_LIST_FILES_LACKS_FILE_NAME,
+    CONTENT_INCORRECT_REQUEST,
     CONTENT_INCORRECT_SINGLE_ARTICLE_ID,
-    CONTENT_INTERNAL_ERROR, CONTENT_INVALID_AMOUNT_EVENTS,
+    CONTENT_INTERNAL_ERROR,
+    CONTENT_INVALID_AMOUNT_EVENTS,
     CONTENT_INVALID_DATE_FORMAT,
     CONTENT_INVALID_PAGE_OR_PAGE_SIZE_TYPE,
     CONTENT_INVALID_PAGE_OR_PAGE_SIZE_VALUE,
-    CONTENT_NO_AUTH_HEADER_PRESENT, CONTENT_NO_MATCH_ANSWER_IS_EMPTY,
+    CONTENT_NO_AUTH_HEADER_PRESENT,
+    CONTENT_NO_MATCH_ANSWER_INCLUDES_SYMBOLS,
+    CONTENT_NO_MATCH_ANSWER_IS_EMPTY,
     CONTENT_NO_RIGHTS_TO_DELETE_ARTICLE,
-    CONTENT_NULL_ARTICLE_FIELDS, CONTENT_NULL_QUESTIONNAIRE_FIELDS,
+    CONTENT_NULL_ARTICLE_FIELDS,
+    CONTENT_NULL_QUESTIONNAIRE_FIELDS,
     CONTENT_PREVIEW_IMAGE_IS_NOT_SUPPORTED_FORMAT,
     CONTENT_PREVIEW_IMAGE_LACKS_FILE_NAME,
-    CONTENT_PREVIEW_IMAGE_LACKS_FILENAME, CONTENT_QUESTION_LIST_IS_EMPTY,
+    CONTENT_PREVIEW_IMAGE_LACKS_FILENAME,
+    CONTENT_QUESTION_LIST_IS_EMPTY,
+    CONTENT_QUESTIONNAIRE_ANSWER_FOR_A_USER_INCLUDES_SYMBOLS,
+    CONTENT_QUESTIONNAIRE_ANSWER_FOR_A_USER_IS_SMALL,
+    CONTENT_QUESTIONNAIRE_ANSWER_LIST_IS_EMPTY, CONTENT_QUESTIONNAIRE_ANSWER_LIST_TEXT_INCLUDES_SYMBOLS,
+    CONTENT_QUESTIONNAIRE_ANSWER_LIST_TEXT_IS_EMPTY,
+    CONTENT_QUESTIONNAIRE_ANSWER_TRIGGERING_WEIGHT_INCLUDES_SYMBOLS,
+    CONTENT_QUESTIONNAIRE_ANSWER_TRIGGERING_WEIGHT_IS_NULL,
+    CONTENT_QUESTIONNAIRE_QUESTION_NUMBER_SHOULD_START_FROM_1,
+    CONTENT_QUESTIONNAIRE_QUESTION_TEXT_INCLUDES_SYMBOLS,
+    CONTENT_QUESTIONNAIRE_QUESTION_TEXT_IS_NULL,
+    CONTENT_QUESTIONNAIRE_TITLE_INCLUDES_SYMBOLS,
+    CONTENT_QUESTIONNAIRE_TITLE_IS_NULL,
+    CONTENT_QUESTIONNAIRE_TITLE_IS_SMALL,
+    CONTENT_QUESTIONNAIRE_TYPE_INCLUDES_SYMBOLS,
+    CONTENT_QUESTIONNAIRE_TYPE_IS_NULL,
     CONTENT_SLIDER_IMAGE_IS_NOT_SUPPORTED_FORMAT,
     CONTENT_SLIDER_IMAGE_IS_NULL,
-    CONTENT_SLIDER_IMAGE_LACKS_FILE_NAME, CONTENT_TITLE_SHOULD_BE_SPECIFIED,
+    CONTENT_SLIDER_IMAGE_LACKS_FILE_NAME,
+    CONTENT_TITLE_SHOULD_BE_SPECIFIED,
     CONTENT_TOO_MANY_FILES,
     CONTENT_USER_DONT_HAVE_EDIT_RIGHTS,
-    CONTENT_USER_DONT_HAVE_MODERATOR_PRIVILEGES, CONTENT_USER_DONT_HAVE_MODERATOR_RIGHTS,
+    CONTENT_USER_DONT_HAVE_MODERATOR_PRIVILEGES,
+    CONTENT_USER_DONT_HAVE_MODERATOR_RIGHTS,
     CONTENT_USER_TOKEN_ERROR,
     SERVER_IS_NOT_RESPONDING
 } from "../../consts/errorText";
@@ -166,6 +190,54 @@ export const setCreateQuestionnaireError = (state, action) => {
                     break;
                 case CONTENT_NULL_QUESTIONNAIRE_FIELDS:
                     state.error = "Обязательные поля не заполнены"
+                    break;
+                case CONTENT_NO_MATCH_ANSWER_INCLUDES_SYMBOLS:
+                    state.error = "Стандартное сообщение не должно содержать символов"
+                    break;
+                case CONTENT_QUESTIONNAIRE_TITLE_IS_NULL:
+                    state.error = "Название анкеты должно быть заполненно"
+                    break;
+                case CONTENT_QUESTIONNAIRE_TITLE_IS_SMALL:
+                    state.error = "Название анкеты должно быть не менее 5-ти символов"
+                    break;
+                case CONTENT_QUESTIONNAIRE_TITLE_INCLUDES_SYMBOLS:
+                    state.error = "Название анкеты может содержать только буквы, пробелы и цифры"
+                    break;
+                case CONTENT_QUESTIONNAIRE_TYPE_IS_NULL:
+                    state.error = "Тип анкеты должен быть заполнен"
+                    break;
+                case CONTENT_QUESTIONNAIRE_TYPE_INCLUDES_SYMBOLS:
+                    state.error = "Тип анкеты может содержать только буквы, пробелы и цифры"
+                    break;
+                case CONTENT_QUESTIONNAIRE_QUESTION_TEXT_IS_NULL:
+                    state.error = "Название всех вопросов должны быть заполненны"
+                    break;
+                case CONTENT_QUESTIONNAIRE_QUESTION_TEXT_INCLUDES_SYMBOLS:
+                    state.error = "Название всех вопросов могут содержать только буквы, пробелы и цифры"
+                    break;
+                case CONTENT_QUESTIONNAIRE_QUESTION_NUMBER_SHOULD_START_FROM_1:
+                    state.error = "Номер вопроса должен быть не менее 1"
+                    break;
+                case CONTENT_QUESTIONNAIRE_ANSWER_FOR_A_USER_IS_SMALL:
+                    state.error = "Сообщение в случае совпадения должно быть не менее 5-ти символов"
+                    break;
+                case CONTENT_QUESTIONNAIRE_ANSWER_FOR_A_USER_INCLUDES_SYMBOLS:
+                    state.error = "Сообщение в случае совпадения может содержать только буквы, пробелы и цифры"
+                    break;
+                case CONTENT_QUESTIONNAIRE_ANSWER_LIST_IS_EMPTY:
+                    state.error = "Список ответов не должен быть пустым, если выбран тип Варианты"
+                    break;
+                case CONTENT_QUESTIONNAIRE_ANSWER_TRIGGERING_WEIGHT_IS_NULL:
+                    state.error = "Ответ для совпадения должен быть заполнен"
+                    break;
+                case CONTENT_QUESTIONNAIRE_ANSWER_TRIGGERING_WEIGHT_INCLUDES_SYMBOLS:
+                    state.error = "Ответ для совпадения может содержать только буквы, пробелы и цифры"
+                    break;
+                case CONTENT_QUESTIONNAIRE_ANSWER_LIST_TEXT_IS_EMPTY:
+                    state.error = "Ответы на вопросы должны быть заполнены"
+                    break;
+                case CONTENT_QUESTIONNAIRE_ANSWER_LIST_TEXT_INCLUDES_SYMBOLS:
+                    state.error = "Ответы на вопросы могут содержать только буквы, пробелы и цифры"
                     break;
                 default:
                     state.error = "Неизвестная ошибка";
