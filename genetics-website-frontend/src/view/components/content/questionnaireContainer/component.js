d58027ed5aa17e011c7c2c64aadc0748c28bdbf9
L1oid <lloidtm@gmail.com>
2024-05-26 14:48:54 +0700

    replacing radio with checkbox in questi solver

--- genetics-website-frontend/src/view/components/content/questionnaireContainer/component.js
+++ genetics-website-frontend/src/view/components/content/questionnaireContainer/component.js
@@ -1,30 +1,27 @@
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import BreadcrumpComponent from "../../common/breadcrump/component";
-import {useParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import SingleContentHeadingComponent from "../../common/singleContentHeading/component";
 import AccountPageSubtitleComponent from "../../common/accountPageSubtitle/component";
 import AccountPageInputComponent from "../../common/accountPageInput/component";
-import {FREE, SELECT} from "../../../../state/consts/contentTypes";
-import {
-    solveQuestionnaire
-} from "../../../../state/slices/content/asyncActions";
+import { FREE, SELECT } from "../../../../state/consts/contentTypes";
+import { solveQuestionnaire } from "../../../../state/slices/content/asyncActions";
 import AccountPageButtonComponent from "../../common/accountPageButton/component";
 import ErrorAndSuccessWindowComponent from "../../common/errorAndSuccessWindow/component";
 
 function QuestionnaireContainerComponent(props) {
-    const {id} = useParams();
+    const { id } = useParams();
 
     const dispatch = useDispatch();
 
     const [questionnaireSolve, setQuestionnaireSolve] = useState({
         questionnaireId: id,
         answerSet: []
-    })
+    });
 
-    const {error, success, status} = useSelector(state => state.content);
+    const { error, success, status } = useSelector(state => state.content);
     const questionnaire = useSelector(state => state.content.questionnaire);
     const questionnaireQuestionsAnswersList = useSelector(state => state.content.questionnaireQuestionsAnswersList);
     const questionnaireQuestionsList = useSelector(state => state.content.questionnaireQuestionsList).slice().sort((a, b) => a.questionNumber - b.questionNumber);
@@ -32,7 +29,9 @@ function QuestionnaireContainerComponent(props) {
     const handleInputChange = (questionId, answerText) => {
         setQuestionnaireSolve(prevState => {
             const newAnswerSet = prevState.answerSet.filter(answer => answer.questionId !== questionId);
-            newAnswerSet.push({questionId, answerText});
+            if (answerText !== '') {
+                newAnswerSet.push({ questionId, answerText });
+            }
             return {
                 ...prevState,
                 answerSet: newAnswerSet
@@ -50,7 +49,7 @@ function QuestionnaireContainerComponent(props) {
     };
 
     const handleConfirmButton = () => {
-        dispatch(solveQuestionnaire(questionnaireSolve))
+        dispatch(solveQuestionnaire(questionnaireSolve));
     };
 
     const ways = [
@@ -66,49 +65,52 @@ function QuestionnaireContainerComponent(props) {
             link: "/questionnaires/" + id,
             name: questionnaire.title
         },
-    ]
+    ];
 
     return (
         <div className="page-container">
             <div className="questionnaire-container">
-                <BreadcrumpComponent ways={ways}/>
-                <SingleContentHeadingComponent title={questionnaire.title}/>
+                <BreadcrumpComponent ways={ways} />
+                <SingleContentHeadingComponent title={questionnaire.title} />
                 <div className="questionnaire-question-list">
                     {questionnaireQuestionsList.map((question, questionIndex) => (
                         <div className="questionnaire-item" key={questionIndex}>
                             <div className="questionnaire-card">
                                 <AccountPageSubtitleComponent
-                                    title={"Вопрос №" + question.questionNumber + (question.answerRequired ? ' *' : '')}/>
+                                    title={"Вопрос №" + question.questionNumber + (question.answerRequired ? ' *' : '')} />
                                 <p className="questionnaire-item-question">
                                     {question.questionText}
                                 </p>
                                 {question.questionType === FREE && (
                                     <div>
                                         <AccountPageSubtitleComponent
-                                            title={"Ответ"}/>
+                                            title={"Ответ"} />
                                         <AccountPageInputComponent
                                             type="text"
                                             value={getAnswerText(question.questionId)}
                                             handle={(e) => handleInputChange(question.questionId, e.target.value)}
                                             placeholder="Ответ"
-                                            disabled={false}/>
+                                            disabled={false} />
                                     </div>
                                 )}
                                 {question.questionType === SELECT && (
                                     <div>
                                         <AccountPageSubtitleComponent
-                                            title={"Ответ"}/>
+                                            title={"Ответ"} />
                                         {getAnswersForQuestion(question.questionId).map((answer, answerIndex) => (
                                             <div key={answerIndex}>
-                                                <input className="questionnaire-card-radio"
-                                                       type="radio"
+                                                <input className="questionnaire-card-checkbox"
+                                                       type="checkbox"
                                                        id={`question-${question.questionId}-answer-${answerIndex}`}
                                                        name={`question-${question.questionId}`}
                                                        value={answer.text}
                                                        checked={getAnswerText(question.questionId) === answer.text}
-                                                       onChange={(e) => handleInputChange(question.questionId, e.target.value)}
+                                                       onChange={(e) => {
+                                                           const isChecked = getAnswerText(question.questionId) === answer.text;
+                                                           handleInputChange(question.questionId, isChecked ? '' : e.target.value);
+                                                       }}
                                                 />
-                                                <label className="questionnaire-card-radio-label"
+                                                <label className="questionnaire-card-checkbox-label"
                                                        htmlFor={`question-${question.questionId}-answer-${answerIndex}`}>
                                                     {answer.text}
                                                 </label>
@@ -124,14 +126,14 @@ function QuestionnaireContainerComponent(props) {
                     <AccountPageButtonComponent
                         status={status}
                         handle={handleConfirmButton}
-                        title={"Подтвердить"}/>
+                        title={"Подтвердить"} />
                 </div>
                 <div className="questionnaire-confirm-button">
-                    <ErrorAndSuccessWindowComponent error={error} success={success}/>
+                    <ErrorAndSuccessWindowComponent error={error} success={success} />
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default QuestionnaireContainerComponent;
